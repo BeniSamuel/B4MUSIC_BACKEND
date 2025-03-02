89aaa2d2b6f2f54421e504a47d4433fcaa241510
benisamuel <benisamuel566@gmail.com>
2025-03-02 21:38:04 +0200

    Established Database Configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config/config.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     AppConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [AppConfigModule],
+      useFactory: ( configService: ConfigService ) => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: +configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService]
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
